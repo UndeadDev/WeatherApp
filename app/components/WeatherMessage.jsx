93e26322725f4eacfc7cd0867f65c01bf4f04c83
Radi <radi.mortada@outlook.com>
2016-12-15 19:33:09 +0200

    copy

--- app/components/WeatherMessage.jsx
+++ app/components/WeatherMessage.jsx
@@ -4,9 +4,6 @@ var React = require('react');
     
 
 var WeatherMessage = ({temp , location}) => {
-    if(location == 'Tharwat' || location =='tharwat'){
-        temp = 'very high in Teez Ahmed Tharwat';
-    }
  return(<h1>Temperture is {temp} ⁰C in {location} </h1>);
 };
 
